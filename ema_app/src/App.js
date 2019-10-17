2c3edffdb3d32b71d913e5a3dc4b1ecd7190d70d
Peter Chen <pcchen@princeton.edu>
2019-10-17 11:16:07 -0400

    Initial commit

--- ema_app/src/App.js
+++ ema_app/src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Graph from "./Graph";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = 'https://api.tiingo.com/tiingo/daily/';
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const dateTitle = "date";
+const closeTitle = "close";
+var headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Token 5c55cabb26b73f033301e8d556368a6acdad40e2',
+        };
+
+var ticker = 'aapl'
+var startDate = '2018-10-04'
+let randomData = () =>
+  Array.from({ length: 11 }, () => Math.floor(Math.random() * 100));
+
+var tempData = {labels: [], datasets: []};
+
+
+class App extends Component {
+  state = {
+    data : {
+      labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"],
+      datasets: [
+        {
+          color: "light-blue",
+          values: randomData()
+        }
+      ]
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Exponential Moving Average Charts</h1>
+        </header>
+        <Graph
+          title="Line Chart"
+          type="line"
+          data={ this.state.data }
+          show_dots={false}
+          heatline
+          region_fill
+        />
+      </div>
+    );
+  }
+  rerender = () => {
+    this.setState({
+      data : tempData
+    });
+  };
+
+  componentDidMount(){
+    fetch(proxyurl + url + ticker + '/' + 'prices?startDate=' + startDate, { method: 'GET', headers: headers})
+    .then(res => res.json())
+      .then(data => 
+        {
+          tempData.datasets.push(({values: []}));
+          data.forEach(day => {
+            tempData.datasets[0].values.push(day[closeTitle]);
+            tempData.labels.push(day[dateTitle]);
+          });
+          this.rerender();
+        }
+      )
+      .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
+	}
 }
 
 export default App;
---
+++ ema_app/src/Graph.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { string, object } from "prop-types";
+import { Chart } from "frappe-charts/dist/frappe-charts.min.esm";
+
+class Graph extends Component {
+  componentDidMount() {
+    const { title, data, type, height = 250, onSelect, ...rest } = this.props;
+    this.graph = new Chart(this.chart, {
+      title,
+      data,
+      type,
+      height,
+      lineOptions: {
+        dotSize: 0.001 // default: 4
+      },
+      is_navigable: !!onSelect,
+      ...rest,
+    });
+    if (onSelect) {
+      this.graph.parent.addEventListener("data-select", onSelect);
+    }
+  }
+  componentWillReceiveProps(props) {
+    this.graph.update(props.data);
+  }
+  render() {
+    return <div ref={chart => (this.chart = chart)} />;
+  }
+}
+
+export default Graph;
+
+Graph.propTypes = {
+  type: string.isRequired,
+  data: object.isRequired
+};
